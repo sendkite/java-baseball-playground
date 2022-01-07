81f558f9c00af19626fd8c26f508e25ccebed7c4
sendkite <sendkite1@gmail.com>
2022-01-07 22:16:54 +0900

    Feature : 파라미터라이즈 테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -2,7 +2,11 @@ package study;
 
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -26,5 +30,17 @@ public class SetTest {
         assertThat(size).isEqualTo(3);
     }
 
+    @ParameterizedTest
+    @DisplayName("트루만 테스트")
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
 
+    @ParameterizedTest
+    @DisplayName("트루던 폴스던 테스트")
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void checkAllContains(int number, boolean expected) {
+        assertThat(numbers.contains(number)).isEqualTo(expected);
+    }
 }
